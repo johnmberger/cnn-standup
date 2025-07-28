2336da896adb284efb5ed031f265ad6f5f4e6697
John Berger <john.berger@wbd.com>
2025-07-28 15:39:50 -0400

    responsive updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,10 @@ import CnnLogo from "../assets/cnn-logo.png";
 export default function Header() {
   return (
     <header className="bg-white p-2 text-black border-b-2 border-black flex flex-row items-center justify-between">
-      <img src={CnnLogo} alt="CNN Logo" className="h-8 w-auto mb-2" />
-      <h1 className="text-2xl font-bold">Media Management Team</h1>
+      <img src={CnnLogo} alt="CNN Logo" className="h-8 w-auto mr-3" />
+      <h1 className="sm:text-2xl text-lg font-bold overflow-ellipsis truncate">
+        Media Management Team
+      </h1>
     </header>
   );
 }
--- src/components/Standup.jsx
+++ src/components/Standup.jsx
@@ -20,15 +20,17 @@ export default function Standup() {
   const standupLeader = getStandupLeaderThisWeek(teamMembers);
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center text-center">
-      <p className="mb-16 text-lg text-gray-600">
+    <div className="min-h-screen -mt-16 p-12 flex flex-col justify-center items-center text-center">
+      <p className="mb-16 sm:text-lg text-md text-gray-600">
         Week of <span className="font-bold">{weekDates}</span>
       </p>
       <div>
-        <p className="mb-8 font-semibold text-5xl">
+        <p className="mb-8 font-semibold sm:text-5xl text-3xl">
           This week's standup leader is
         </p>
-        <p className="font-bold text-9xl text-red-600">{standupLeader}</p>
+        <p className="font-bold sm:text-9xl text-7xl text-red-600">
+          {standupLeader}
+        </p>
       </div>
     </div>
   );
